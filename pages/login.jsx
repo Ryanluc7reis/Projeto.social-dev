e36d963012ea0c7431f6ab220d636fe51ec15715
Ryan Lucas <ryanlucasfrfrfrfr@hotmail.com>
2023-02-14 00:15:53 -0300

    Criado components de imagem

---
+++ pages/login.jsx
@@ -0,0 +1,12 @@
+import ImageWithSpace from '../src/components/layout/ImageWithSpace'
+
+
+function LoginPage () {
+  return (
+    <ImageWithSpace>
+      teste
+    </ImageWithSpace>
+  )
+
+}
+export default LoginPage
