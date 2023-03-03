5ec5aa0086f12fc94c6277bf80289350f116cd7b
Ryan Lucas <ryanlucasfrfrfrfr@hotmail.com>
2023-03-03 18:34:04 -0300

    Adicionado joi e validadacao no signup

--- modules/user/user.service.js
+++ modules/user/user.service.js
@@ -1,5 +1,6 @@
-export const signupUser = () => { //quando a função é somente export const ,deve importar assim : import {signupUser} from 
+export const signupUser = (body) => { //quando a função é somente export const ,deve importar assim : import {signupUser} from 
   console.log('SIGNUP USER')
+  console.log(body)
   return true
 
 }
