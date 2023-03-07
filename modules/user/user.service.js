d74d4e017ec990a8790fe8e0dc03b90218ac0504
Ryan Lucas <ryanlucasfrfrfrfr@hotmail.com>
2023-03-07 17:34:59 -0300

    Criado model de user

--- modules/user/user.service.js
+++ modules/user/user.service.js
@@ -1,7 +1,17 @@
-export const signupUser = (body) => { //quando a função é somente export const ,deve importar assim : import {signupUser} from 
-  console.log('SIGNUP USER')
-  console.log(body)
-  return true
+import { hashPassword } from "../../utils/bcrypt"
 
+import User from './user.model'
+
+export const signupUser = async (body) => { //quando a função é somente export const ,deve importar assim : import {signupUser} from 
+  try {
+    const user = {
+      ...body,
+      password: hashPassword(body.password)
+    }
+    const dbUser = await User.create(user)
+    return dbUser
+  } catch (err) {    
+    throw err
+  }
 }
  
