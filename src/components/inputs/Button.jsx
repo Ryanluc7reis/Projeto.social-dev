a655a1830004ef191aec56f70e927dfff1f53ecb
Ryan Lucas <ryanlucasfrfrfrfr@hotmail.com>
2023-03-10 00:19:05 -0300

    Criado tratamento de erros para o cadastro

--- src/components/inputs/Button.jsx
+++ src/components/inputs/Button.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "styled-components"
  
 const Button = styled.button`
   background-color : ${props => props.theme.primary};
@@ -6,13 +6,18 @@ const Button = styled.button`
   border-radius: 10px;
   border: 0;
   font-weight:bold;
-  color:${props => props.theme.white}
+  color:${props => props.theme.white};
   font-size: 16px;
-  cursor: pointer;
   transition: 0.4s;
 
-  :hover{
-    background-color: ${props => props.theme.primaryHover}
+  ${props => !props.disabled && 'cursor: pointer;'} 
+
+  :hover {
+    background-color: ${props => props.theme.primaryHover};
+  }
+  :disabled {
+    background-color : ${props => props.theme.disabled};
   }
 `
 export default Button
+// '${props => !props.disabled && 'cursor: pointer;'} = SE NAO estiver desabilitador ,aparecer cursor
--- src/components/inputs/Input.jsx
+++ src/components/inputs/Input.jsx
@@ -18,13 +18,30 @@ const StyledInput = styled.input`
   padding: 15px 20px;
   box-sizing: border-box ;
   border-radius : 10px;
-`
 
-const Input = forwardRef (({label, ...props}, ref) => { // "...props " significa q todas as props que nao for label ,ira substituir automaticamente todas as outras props
-  return (                                               // o parametro do "forwardRef" é o componente todo como esta a abaixo         
+    &:focus {
+    outline: none;
+  }
+  ${props => props.error && `border: 2px solid ${props => props.theme.error};`}
+`
+const ErrorLabel = styled.span`
+  color: ${props => props.theme.error};
+  font-weight: bold;
+  font-size: 14px;
+ 
+
+`
+const errorMessage = {
+  'string.empty': 'Este campo é obrigatório.',
+  'string.email':'Por favor,digite um email válido.'
+}
+const Input = forwardRef (({label, error, ...props }, ref) => {
+                                                                    
+  return (                                                            // o parametro do "forwardRef" é o componente todo como esta a abaixo      // "...props " significa q todas as props que nao for label ,ira substituir automaticamente todas as outras props   
      <InputContainer>
          <StyledLabel> {label}</StyledLabel>
-         <StyledInput placeholder={label} {...props} ref={ref} />
+         <StyledInput placeholder={label} error={error}  {...props} ref={ref}  />
+         {error && <ErrorLabel>{errorMessage[error.type] || error.message}</ErrorLabel>}
      </InputContainer>
   )
   
